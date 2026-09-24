e7138f10b8e77f751b97ae5b6ff38fe68b267c4f
OmariSyn <sonicknight3000@gmail.com>
2026-09-23 21:45:52 -0400

    Add files via upload

---
+++ Controllers/PersonController.cs
@@ -0,0 +1,29 @@
+﻿using ReactAPIMobile.DataAccess;
+using ReactAPIMobile.Models;
+using Microsoft.AspNetCore.Mvc;
+using ReactAPIMobile.DataAccess;
+using ReactAPIMobile.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+namespace ReactAPIMobile.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        // GET: api/<PersonController>
+        [HttpGet]
+        public IEnumerable<Person> Get()
+        {
+            return new PersonData().GetPeople();
+        }
+
+        // POST api/<PersonController>
+        [HttpPost]
+        public void Post([FromBody] Person value)
+        {
+            var pd = new PersonData();
+            pd.SavePerson(value);
+        }
+    }
+}
---
+++ DataAccess/PersonData.cs
@@ -0,0 +1,40 @@
+﻿using SQLite;
+using ReactAPIMobile.Models;
+
+
+namespace ReactAPIMobile.DataAccess
+{
+    public class PersonData
+    {
+        SQLiteConnection database;
+
+        public void Init()
+        {
+            if (database is not null)
+            {
+                return;
+            }
+            database = new SQLiteConnection(DatabaseConstants.DatabasePath, DatabaseConstants.Flags);
+            database.CreateTable<Person>();
+        }
+        public List<Person> GetPeople()
+        {
+            Init();
+            return database.Table<Person>().ToList();
+        }
+        public int SavePerson(ReactAPIMobile.Models.Person person)
+        {
+            Init();
+            if (person.ID != 0)
+            {
+                // Update an existing person
+                return database.Update(person);
+            }
+            else
+            {
+                // Save a new person
+                return database.Insert(person);
+            }
+        }
+    }
+}
